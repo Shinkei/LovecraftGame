d343ca01a21c548a8825a0ba8e51461d60ae2696
Shinkei <ing.jorgeivanramirezh@gmail.com>
2017-11-04 22:24:55 -0500

    add image to the phaser game

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ import 'pixi.js';
 import 'p2';
 import { Phaser } from 'phaser';
 
+import BACKGROUND from './../assets/background.jpg';
+
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 // states of the game
 var gameState = {
     preload: function() { // where all the requirements are loaded before the game starts
-        
+        this.load.image('background', BACKGROUND);
     },
     create: function() { // the game starts
-        
+        this.background = this.game.add.sprite(0, 0, 'background');
     },
     update: function() { // every change the game excecutes this method
         
